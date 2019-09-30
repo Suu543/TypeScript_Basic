5245f3fa17ed8ef659bf9abe11cc8a59ec3fd576
jos50275266 <jos50275266@gmail.com>
2019-10-01 05:09:51 +0900

    Optional Implements Clauses

--- 09_Design_Pattern/maps/dist/src.f10117fe.js
+++ 09_Design_Pattern/maps/dist/src.f10117fe.js
@@ -85113,6 +85113,7 @@ var User =
 /** @class */
 function () {
   function User() {
+    this.color = "red";
     this.name = faker_1.default.name.firstName();
     this.location = {
       // convert string type to number
@@ -85198,6 +85199,7 @@ var Company =
 /** @class */
 function () {
   function Company() {
+    this.color = "red";
     this.companyName = faker_1.default.company.companyName();
     this.catchPhrase = faker_1.default.company.catchPhrase();
     this.location = {
