fd48b327fa205f587327ff880bf81cbfae650766
jos50275266 <jos50275266@gmail.com>
2019-10-01 04:51:29 +0900

    Restricting Access with interfaces and Implicit Type Checks

--- 09_Design_Pattern/maps/src/index.ts
+++ 09_Design_Pattern/maps/src/index.ts
@@ -9,6 +9,14 @@
 
 // Global Variable로 google을 이용할 수 있다.
 
+import { User } from "./User";
 import { CustomMap } from "./CustomMap";
+import { Company } from "./Company";
 
-new CustomMap("map");
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
+
+// Implicit Check User and Company가 addMarker Method의 interface를 충족하는지 Typescript이 자동으로 확인
+customMap.addMarker(user);
+customMap.addMarker(company);
