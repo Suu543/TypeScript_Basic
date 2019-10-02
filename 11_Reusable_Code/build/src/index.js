fc743e792d977cf77be2a37fcae4a1b6478d415a
jos50275266 <jos50275266@gmail.com>
2019-10-03 02:18:43 +0900

    Using Enums

--- 11_Reusable_Code/build/src/index.js
+++ 11_Reusable_Code/build/src/index.js
@@ -13,17 +13,26 @@ var matches = fs_1.default.readFileSync('football.csv', {
     .map(function (row) {
     return row.split(',');
 });
-var homeWin = 'H';
-var awayWin = 'A';
-var draw = 'D';
+// Draw가 사용되지않았다고 지우면 모든 dataset을 알지못하기 때문에 이 문제를 방지하기위해 ENUM을 사용하자
+// Object는 다양한 용도로 사용되는데 이 경우 정확이 어떤 용례로 사용했는지 명시하기위해 enum keyword 사용하자
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
 var manUnitedWins = 0;
 // Analyze - hard to understand What each string means
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
-    if (match[1] === "Man United" && match[5] === homeWin) {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === "Man United" && match[5] === awayWin) {
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- 11_Reusable_Code/src/index.ts
+++ 11_Reusable_Code/src/index.ts
@@ -10,18 +10,27 @@ const matches = fs.readFileSync('football.csv', {
         return row.split(',');
     });
 
-const homeWin = 'H';
-const awayWin = 'A';
-const draw = 'D';
+// Draw가 사용되지않았다고 지우면 모든 dataset을 알지못하기 때문에 이 문제를 방지하기위해 ENUM을 사용하자
+// Object는 다양한 용도로 사용되는데 이 경우 정확이 어떤 용례로 사용했는지 명시하기위해 enum keyword 사용하자
+// enum - enumeration
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = "D"
+}
+
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
 
 
 let manUnitedWins = 0;
 
 // Analyze - hard to understand What each string means
 for (let match of matches) {
-    if (match[1] === "Man United" && match[5] === homeWin) {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
         manUnitedWins++
-    } else if (match[2] === "Man United" && match[5] === awayWin) {
+    } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
