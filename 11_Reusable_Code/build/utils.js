15d83da536e2d57eeab878c102cd55c7c07d413c
jos50275266 <jos50275266@gmail.com>
2019-10-03 02:47:57 +0900

    Converting Date Strings to Dates

---
+++ 11_Reusable_Code/build/utils.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.dateStringToDate = function (datString) {
+    // 28/10/2018
+    var dateParts = datString.split('/') /* ['28', '10', '2018'] */
+        .map(function (value) {
+        return parseInt(value);
+    });
+    // January is supposed to be zero
+    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};
---
+++ 11_Reusable_Code/src/utils.ts
@@ -0,0 +1,9 @@
+export const dateStringToDate = (datString: string): Date => {
+    // 28/10/2018
+    const dateParts = datString.split('/') /* ['28', '10', '2018'] */
+        .map((value: string): number => {
+            return parseInt(value)
+        });
+    // January is supposed to be zero
+    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0])
+}
